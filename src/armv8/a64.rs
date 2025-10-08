f05860f6fd699cf2f32d6b61837ecd16467da65f
Martin Fink <martin@finkmartin.com>
2025-10-08 19:06:42 +0200

    fix opcode display for ldg

--- src/armv8/a64.rs
+++ src/armv8/a64.rs
@@ -2535,7 +2535,7 @@ impl Display for Opcode {
             Opcode::TBX => "tbx",
             Opcode::FCADD => "fcadd",
             Opcode::LDGM => "ldgm",
-            Opcode::LDG => "ldm",
+            Opcode::LDG => "ldg",
             Opcode::STGM => "stgm",
             Opcode::STZGM => "stzgm",
             Opcode::STG => "stg",
