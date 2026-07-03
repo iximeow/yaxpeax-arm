8da36524a7eb3d84aad140bb0ba1cca905f1df76
Brandon Ros <brandonros1@gmail.com>
2026-07-03 00:14:45 -0400

    fix more thumb2 register data-processing decode bugs
    - register-controlled shifts (lsl/lsr/asr/ror by register) all decoded as
      lsl: the shift type was read from hw1[6:5] (always 0 for this form)
      instead of hw0[6:5]
    - 32-bit uxt*/sxt* extends read the ror amount from the wrong field
      (hw1[2:1] scaled by 4 instead of hw1[5:4] scaled by 8), so a no-rotation
      extend came back as ror #4; rotation of 0 is now omitted
    - sxtab was decoded as sxtah (wrong opcode table entry)
    
    all cases verified against binutils objdump

--- src/armv7/thumb.rs
+++ src/armv7/thumb.rs
@@ -2856,7 +2856,7 @@ pub fn decode_into<T: Reader<<ARMv7 as Arch>::Address, <ARMv7 as Arch>::Word>>(d
                                     Opcode::LSR,
                                     Opcode::ASR,
                                     Opcode::ROR,
-                                ][op2[1..3].load::<usize>()];
+                                ][op1[1..3].load::<usize>()];
                                 let rd = lower2[8..12].load::<u8>();
                                 let rm = lower2[0..4].load::<u8>();
                                 inst.opcode = op;
@@ -2884,14 +2884,18 @@ pub fn decode_into<T: Reader<<ARMv7 as Arch>::Address, <ARMv7 as Arch>::Word>>(d
                                     ][op1];
 
                                     let rm = lower2[..4].load::<u8>();
-                                    let rotate = lower2[1..3].load::<u8>() << 2;
+                                    let rotate = lower2[4..6].load::<u8>() << 3;
                                     let rd = lower2[8..12].load::<u8>();
 
                                     inst.opcode = op;
                                     inst.operands = [
                                         Operand::Reg(Reg::from_u8(rd)),
                                         Operand::Reg(Reg::from_u8(rm)),
-                                        Operand::Imm32(rotate as u32),
+                                        if rotate != 0 {
+                                            Operand::Imm32(rotate as u32)
+                                        } else {
+                                            Operand::Nothing
+                                        },
                                         Operand::Nothing,
                                     ];
                                 } else {
@@ -2900,12 +2904,12 @@ pub fn decode_into<T: Reader<<ARMv7 as Arch>::Address, <ARMv7 as Arch>::Word>>(d
                                         Opcode::UXTAH,
                                         Opcode::SXTAB16,
                                         Opcode::UXTAB16,
-                                        Opcode::SXTAH,
+                                        Opcode::SXTAB,
                                         Opcode::UXTAB,
                                     ][op1];
 
                                     let rm = lower2[..4].load::<u8>();
-                                    let rotate = lower2[1..3].load::<u8>() << 2;
+                                    let rotate = lower2[4..6].load::<u8>() << 3;
                                     let rd = lower2[8..12].load::<u8>();
 
                                     inst.opcode = op;
@@ -2913,7 +2917,11 @@ pub fn decode_into<T: Reader<<ARMv7 as Arch>::Address, <ARMv7 as Arch>::Word>>(d
                                         Operand::Reg(Reg::from_u8(rd)),
                                         Operand::Reg(Reg::from_u8(rn)),
                                         Operand::Reg(Reg::from_u8(rm)),
-                                        Operand::Imm32(rotate as u32),
+                                        if rotate != 0 {
+                                            Operand::Imm32(rotate as u32)
+                                        } else {
+                                            Operand::Nothing
+                                        },
                                     ];
                                 };
                             }
