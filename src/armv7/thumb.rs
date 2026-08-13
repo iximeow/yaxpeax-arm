f89db8330cb08c435c460b1b574f951ea544b8be
Felipe Martinez <felipe@pipe01.net>
2026-08-13 17:51:25 +0200

    Fix B instruction offset

--- src/armv7/thumb.rs
+++ src/armv7/thumb.rs
@@ -4031,7 +4031,7 @@ pub fn decode_into<T: Reader<<ARMv7 as Arch>::Address, <ARMv7 as Arch>::Word>>(d
                 let imm = instr2[0..8].load::<u8>() as i8 as i32;
                 inst.condition = ConditionCode::build(opcode);
                 inst.operands = [
-                    Operand::BranchThumbOffset(imm + 1),
+                    Operand::BranchThumbOffset(imm << 1),
                     Operand::Nothing,
                     Operand::Nothing,
                     Operand::Nothing,
