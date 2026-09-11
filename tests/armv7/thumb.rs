0eb1d9d89c78c6a5b0f49951b233a8ad63b62f5b
iximeow <me@iximeow.net>
2026-09-11 16:59:31 +0000

    that case is not unpredictable anymore!

--- tests/armv7/thumb.rs
+++ tests/armv7/thumb.rs
@@ -91,11 +91,6 @@ fn test_nonconforming(data: &[u8], expected: &'static str) {
 }
 
 #[test]
-fn test_unpredictable_instructions() {
-    test_invalid(&[0x80, 0xfa, 0x40, 0x00]);
-}
-
-#[test]
 fn test_decode_add_cases() {
     test_display(
         &[0x01, 0x44],
